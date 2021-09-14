543085da061ce74e4598d6a93d8dafb595d51e00
ROHIT\Rohit <maanasa0318@gmail.com>
2021-09-14 14:45:20 -0400

    code

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/maanasasubrahmanyam-sd/test
 go 1.16
 
 require (
-	github.com/gogo/protobuf v1.3.2
-	github.com/golang/protobuf v1.5.2
+	github.com/gogo/protobuf v1.3.0
+	github.com/golang/protobuf v1.5.0
+	google.golang.org/protobuf v1.27.1 // indirect
 )
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -10,7 +10,7 @@ import (
 	"github.com/gogo/protobuf/proto"
 	"github.com/gogo/protobuf/protoc-gen-gogo/descriptor"
 	"github.com/gogo/protobuf/protoc-gen-gogo/generator"
-	validator "github.com/maanasasubrahmanyam-sd/test/validator"
+	validator "github.com/maanasasubrahmanyam-sd/test"
 )
 
 const alphaPattern = "^[a-zA-Z]+$"
