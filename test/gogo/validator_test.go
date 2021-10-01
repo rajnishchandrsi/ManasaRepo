4a4bfcc592654a4acd157b76f3068d651f360355
ROHIT\Rohit <maanasa0318@gmail.com>
2021-10-01 12:51:35 -0400

    code

--- test/gogo/validator_test.go
+++ test/gogo/validator_test.go
@@ -7,11 +7,12 @@ import (
 
 
 
-func buildProto3(AlphaTrue, AlphaFalse string) *ValidatorMessage3 {
+func buildProto3(AlphaTrue, AlphaFalse, Noval string) *ValidatorMessage3 {
 
 	goodProto3 := &ValidatorMessage3{
 		AlphaTrue:  AlphaTrue,
 		AlphaFalse: AlphaFalse,
+		Noval: Noval,
 	}
 
 	return goodProto3
@@ -19,7 +20,7 @@ func buildProto3(AlphaTrue, AlphaFalse string) *ValidatorMessage3 {
 
 func TestStringRegex(t *testing.T) {
 	var isTestCasePass = true
-	tooLong1Proto3 := buildProto3("hello", "test123")
+	tooLong1Proto3 := buildProto3("hello", "test123^^&*&(*", "test")
 	if len(tooLong1Proto3.Secvalidator()) > 0 {
 		for _, err := range tooLong1Proto3.Secvalidator() {
 			fmt.Println(err)
@@ -35,13 +36,12 @@ func TestStringRegex(t *testing.T) {
 
 func TestStringRegex2(t *testing.T) {
 	var isTestCasePass = true
-	tooLong2Proto3 := buildProto3("hello#$%", "test123 (&*")
+	tooLong2Proto3 := buildProto3("hello#$%", "test123 (&*", "test")
 	if len(tooLong2Proto3.Secvalidator()) > 0 {
 		for _, err := range tooLong2Proto3.Secvalidator() {
 			fmt.Println(err)
 		}
 		isTestCasePass = false
-
 	}
 
 	if !isTestCasePass {
