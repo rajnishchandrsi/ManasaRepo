71bf243d8e0b101b1459521ef45742fdd79c2c98
ROHIT\Rohit <maanasa0318@gmail.com>
2021-10-04 12:08:46 -0400

    code

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -14,7 +14,7 @@ import (
 
 const alphaPattern = "^[a-zA-Z()\\[\\]\\-_`'\" \\n\\r\\t&]+$"
 const defaultPattern = "^[a-z']+$"
-const betaPattern = "^[a-zA-Z0-9]+$"
+const betaPattern = "^[a-zA-Z0-9\\[\\]]+$"
 
 type plugin struct {
 	*generator.Generator
@@ -70,20 +70,19 @@ func (p *plugin) generateRegexVars(file *generator.FileDescriptor, message *gene
 		if validator != nil {
 			fieldName := p.GetOneOfFieldName(message, field)
 			if validator.Alpha != nil && *validator.Alpha {
-				alphaPatternStr := strings.Replace(alphaPattern, `\`, `\\`, -1) 
+				alphaPatternStr := strings.Replace(alphaPattern, `\`, `\\`, -1)
 				alphaPatternStr = strings.Replace(alphaPatternStr, `"`, `\"`, -1)
 
 				p.P(`var `, p.regexName(ccTypeName, fieldName), ` = `, p.regexPkg.Use(), `.MustCompile(`, "\"", alphaPatternStr, "\"", `)`)
 			} else if validator.Beta != nil && *validator.Beta {
-				betaPatternStr := strings.Replace(betaPattern, `"`, `\"`, -1)
+				betaPatternStr := strings.Replace(betaPattern, `\`, `\\`, -1)
 				p.P(`var `, p.regexName(ccTypeName, fieldName), ` = `, p.regexPkg.Use(), `.MustCompile(`, "\"", betaPatternStr, "\"", `)`)
 			} else {
 				// no validation
 			}
 		} else {
 			fieldName := p.GetOneOfFieldName(message, field)
-			defaultPatternStr := strings.Replace(defaultPattern, `"`, `\"`, -1)
-			p.P(`var `, p.regexName(ccTypeName, fieldName), ` = `, p.regexPkg.Use(), `.MustCompile(`, "\"", defaultPatternStr, "\"", `)`)
+			p.P(`var `, p.regexName(ccTypeName, fieldName), ` = `, p.regexPkg.Use(), `.MustCompile(`, "\"", defaultPattern, "\"", `)`)
 		}
 	}
 }
