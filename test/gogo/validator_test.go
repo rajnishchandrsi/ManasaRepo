00cc851dbdb998b02b53969437806ec9e8851cfe
ROHIT\Rohit <maanasa0318@gmail.com>
2021-10-06 11:27:44 -0400

    code

--- test/gogo/validator_test.go
+++ test/gogo/validator_test.go
@@ -5,11 +5,12 @@ import (
 	"testing"
 )
 
-func buildProto3(AlphaTrue, AlphaFalse, Noval string) *ValidatorMessage3 {
+func buildProto3(AlphaTrue, AlphaFalse, Beta, Noval string) *ValidatorMessage3 {
 
 	goodProto3 := &ValidatorMessage3{
 		AlphaTrue:  AlphaTrue,
 		AlphaFalse: AlphaFalse,
+		Beta:       Beta,
 		Noval:      Noval,
 	}
 
@@ -18,7 +19,7 @@ func buildProto3(AlphaTrue, AlphaFalse, Noval string) *ValidatorMessage3 {
 
 func TestStringRegex(t *testing.T) {
 	var isTestCasePass = true
-	tooLong1Proto3 := buildProto3("h_-ello[]'\"`\t\n\r &", "test123^^&*&(*", "test")
+	tooLong1Proto3 := buildProto3("h_-ello[]'\"`\t\n\r &", "test123^^&*&(*", "[2]a", "test")
 	if len(tooLong1Proto3.Secvalidator()) > 0 {
 		for _, err := range tooLong1Proto3.Secvalidator() {
 			fmt.Println(err)
