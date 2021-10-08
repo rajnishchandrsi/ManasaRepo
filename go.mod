8105c462f5989820525c21a262ddab0cc8ad3902
ROHIT\Rohit <maanasa0318@gmail.com>
2021-10-08 13:52:17 -0400

    code

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/maanasasubrahmanyam-sd/test
+module github.com/maanasasubrahmanyam-sd/test/secvalidator
 
 go 1.16
 
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gogo/protobuf/proto"
 	"github.com/gogo/protobuf/protoc-gen-gogo/descriptor"
 	"github.com/gogo/protobuf/protoc-gen-gogo/generator"
-	validator "github.com/maanasasubrahmanyam-sd/test/secvalidator"
+	validator "github.com/maanasasubrahmanyam-sd/test"
 )
 
 const alphaPattern = "^[a-zA-Z()\\[\\]\\-_`'\" \\n\\r\\t&]+$"
@@ -41,7 +41,7 @@ func (p *plugin) Generate(file *generator.FileDescriptor) {
 	p.PluginImports = generator.NewPluginImports(p.Generator)
 	p.regexPkg = p.NewImport("regexp")
 	p.fmtPkg = p.NewImport("fmt")
-	p.validatorPkg = p.NewImport("github.com/maanasasubrahmanyam-sd/test/secvalidator")
+	p.validatorPkg = p.NewImport("github.com/maanasasubrahmanyam-sd/test")
 	fmt.Fprintln(os.Stderr, "imports", p)
 	for _, msg := range file.Messages() {
 		if msg.DescriptorProto.GetOptions().GetMapEntry() {
